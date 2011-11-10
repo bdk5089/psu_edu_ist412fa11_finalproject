ec47cd7f75a37061f6ae8072d269f0b0cfc03fc8
Bruce K <brucedkennedy@gmail.com>
2011-11-09 20:44:58 -0700

    Some classes

---
+++ src/edu/psu/ist412/junit/testGame.java
@@ -0,0 +1,17 @@
+package edu.psu.ist412.junit;
+
+import junit.framework.TestCase;
+
+public class testGame extends TestCase {
+
+	public testGame(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+	}
+
+	protected void tearDown() throws Exception {
+	}
+
+}
---
+++ src/edu/psu/ist412/view/Program.java
@@ -0,0 +1,13 @@
+package edu.psu.ist412.view;
+
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
