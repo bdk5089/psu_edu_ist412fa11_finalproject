742c939ea9d124825c3e54addfe25ef97892e0af
Bruce K <brucedkennedy@gmail.com>
2011-11-12 15:39:55 -0700

    New test

---
+++ src/edu/psu/ist412/test/GameControllerTest.java
@@ -0,0 +1,19 @@
+package edu.psu.ist412.test;
+
+import junit.framework.TestCase;
+
+public class GameControllerTest extends TestCase {
+
+	public GameControllerTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
