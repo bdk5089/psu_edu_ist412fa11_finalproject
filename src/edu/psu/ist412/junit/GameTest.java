5875eeb8405007d52cf83940c1475c7e0f6f58ec
Bruce K <brucedkennedy@gmail.com>
2011-11-12 15:19:05 -0700

    Rename

---
+++ src/edu/psu/ist412/junit/GameTest.java
@@ -0,0 +1,17 @@
+package edu.psu.ist412.junit;
+
+import junit.framework.TestCase;
+
+public class GameTest extends TestCase {
+
+	public GameTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+	}
+
+	protected void tearDown() throws Exception {
+	}
+
+}
