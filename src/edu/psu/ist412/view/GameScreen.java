7753b97c6aa64d181b5e02366a4dc8a64c02b779
Jeff Rockwell <jeff.rockwell07@gmail.com>
2011-11-27 22:16:32 -0500

    Added the ability for the game screen to update dynamically.  Added new game and next buttons along with their associated functionality.

--- src/edu/psu/ist412/view/GameScreen.java
+++ src/edu/psu/ist412/view/GameScreen.java
@@ -1,10 +1,13 @@
 package edu.psu.ist412.view;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -14,6 +17,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import edu.psu.ist412.poker.Card;
+import edu.psu.ist412.poker.Game;
 import edu.psu.ist412.poker.GameController;
 
 
@@ -23,15 +27,28 @@ public class GameScreen extends JFrame{
 	private final JMenu fileMenu = new JMenu("File");
 	private final JMenuItem logoutItem = new JMenuItem("Logout");
 	
+	private JButton nextButton = new JButton("Next");
+	private JButton newGameButton = new JButton("New Game");
+	
+	private JLabel card1 = new JLabel();
+	private JLabel card2 = new JLabel();
+	private JLabel card3 = new JLabel();
+	private JLabel card4 = new JLabel();
+	private JLabel card5 = new JLabel();
+	
 	//TODO:  determine if we're going to have the save option
 	//private final JMenuItem saveLogoutItem = new JMenuItem("Save and Logout");
 	
 	private final JMenuItem statisticsItem = new JMenuItem("Show/Hide Statistics");
 	
+	final JPanel statisticsPanel = statisticsPanel();
+	
 	private boolean showStatistics = false;
 	
 	// a reference to the login screen
 	private JFrame loginScreen;
+	
+	private JPanel gamePanel;
 	
 	GameController gc;
 
@@ -41,25 +58,56 @@ public class GameScreen extends JFrame{
 		loginScreen = login;
 		
 		gc = controller;
-		gc.createGame();
 		
-		getContentPane().setLayout(new BorderLayout());
-		
-		final JPanel statisticsPanel = statisticsPanel();
-		
-		add(cpuPanel(), BorderLayout.NORTH);
-		add(probabilityPanel(), BorderLayout.EAST);
-		add(playerPanel(), BorderLayout.SOUTH);
-		add(statisticsPanel, BorderLayout.WEST);
-		add(communityPanel(), BorderLayout.CENTER);
-		
-		statisticsPanel.setVisible(false);
+		newGame();
 		
 		setJMenuBar(menuBar);
 		menuBar.add(fileMenu);
 		fileMenu.add(statisticsItem);
 		fileMenu.add(logoutItem);
 		//fileMenu.add(saveLogoutItem);
+		
+		newGameButton.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent event) {
+						newGame();
+					}
+				}
+			);
+			
+			nextButton.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent event) {
+						Game currentGame = gc.getCurrentGame();
+						
+						switch (currentGame.getState()) {
+						case START:
+							currentGame.dealFlop();
+							card1.setVisible(true);
+							card2.setVisible(true);
+							card3.setVisible(true);
+							show();
+							currentGame.setState(Game.GameState.FLOP);
+							break;
+						case FLOP:
+							currentGame.dealTurn();
+							card4.setVisible(true);
+							show();
+							currentGame.setState(Game.GameState.TURN);
+							break;
+						case TURN:
+							currentGame.dealRiver();
+							card5.setVisible(true);
+							show();
+							currentGame.setState(Game.GameState.RIVER);
+							nextButton.setEnabled(false);
+							break;
+						case RIVER:
+							break;
+						}
+					}
+				}
+			);
 		
 		logoutItem.addActionListener(
 			new ActionListener() {
@@ -105,6 +153,34 @@ public class GameScreen extends JFrame{
 		
 	}
 	
+	private JPanel gamePanel() {
+		JPanel gamePanel = new JPanel();
+		
+		gc.createGame();
+		
+		gamePanel.setLayout(new BorderLayout());
+		
+		gamePanel.add(cpuPanel(), BorderLayout.NORTH);
+		gamePanel.add(probabilityPanel(), BorderLayout.EAST);
+		gamePanel.add(southPanel(), BorderLayout.SOUTH);
+		gamePanel.add(statisticsPanel, BorderLayout.WEST);
+		gamePanel.add(communityPanel(), BorderLayout.CENTER);
+		
+		statisticsPanel.setVisible(false);
+		
+		return gamePanel;
+	}
+	
+	private void newGame() {
+		getContentPane().removeAll();
+		gamePanel = gamePanel();
+		add(gamePanel);
+		show();
+		
+		gc.getCurrentGame().setState(Game.GameState.START);
+		nextButton.setEnabled(true);
+	}
+	
 	/**
 	 * Gets rid of the game window, and brings back the login screen.
 	 */
@@ -125,6 +201,28 @@ public class GameScreen extends JFrame{
 			card.setIcon(c.getImage());
 			panel.add(card);
 		}
+		
+		return panel;
+	}
+	
+	private JPanel buttonPanel() {
+		JPanel panel = new JPanel();
+		
+		panel.setLayout(new FlowLayout());
+		
+		panel.add(newGameButton);
+		panel.add(nextButton);
+		
+		return panel;
+	}
+	
+	private JPanel southPanel() {
+		JPanel panel = new JPanel();
+		
+		panel.setLayout(new GridLayout(2,1));
+		
+		panel.add(playerPanel());
+		panel.add(buttonPanel());
 		
 		return panel;
 	}
@@ -152,11 +250,33 @@ public class GameScreen extends JFrame{
 		
 		panel.setLayout(new FlowLayout());
 		
-		for (Card c : gc.getCurrentGame().getTable().getCards()) {
-			JLabel card = new JLabel();
-			card.setIcon(c.getImage());
-			panel.add(card);
-		}
+		ArrayList<Card> cards = gc.getCurrentGame().getTableCards();
+		
+		card1.setIcon(cards.get(0).getImage());
+		panel.add(card1);
+		card1.setVisible(false);
+		
+		card2.setIcon(cards.get(1).getImage());
+		panel.add(card2);
+		card2.setVisible(false);
+		
+		card3.setIcon(cards.get(2).getImage());
+		panel.add(card3);
+		card3.setVisible(false);
+		
+		card4.setIcon(cards.get(3).getImage());
+		panel.add(card4);
+		card4.setVisible(false);
+		
+		card5.setIcon(cards.get(4).getImage());
+		panel.add(card5);
+		card5.setVisible(false);
+		
+//		for (Card c : gc.getCurrentGame().getTableCards()) {
+//			JLabel card = new JLabel();
+//			card.setIcon(c.getImage());
+//			panel.add(card);
+//		}
 		
 		return panel;
 	}
