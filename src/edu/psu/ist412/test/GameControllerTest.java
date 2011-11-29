9cf7f4329371eee9233cfc711329b7d2600d4671
Pam Edmonston <pss138@gmail.com>
2011-11-29 14:10:49 -0500

    First draft of Junit tests for GameController and Table

--- src/edu/psu/ist412/test/GameControllerTest.java
+++ src/edu/psu/ist412/test/GameControllerTest.java
@@ -1,9 +1,22 @@
 package edu.psu.ist412.test;
 
+import java.util.ArrayList;
+
+import edu.psu.ist412.poker.Game;
+import edu.psu.ist412.poker.GameController;
+import edu.psu.ist412.poker.Player;
 import junit.framework.TestCase;
 
-public class GameControllerTest extends TestCase {
+/**
+ * JUnit test for the GameController class/methods
+ * @author PEdmonston
+ */
 
+public class GameControllerTest extends TestCase {
+	GameController gc = new GameController();
+	ArrayList<Game> games = new ArrayList<Game>();
+	ArrayList<Player> players = new ArrayList<Player>();
+	
 	public GameControllerTest(String name) {
 		super(name);
 	}
@@ -15,5 +28,12 @@ public class GameControllerTest extends TestCase {
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
-
+	public void testGameController() {
+		gc.createGame();
+		assertTrue(gc.getCurrentGame() instanceof Game);
+		System.out.println(games.size());
+		System.out.println(players.size());
+//		assertFalse(games.size() == 1);
+//		assertTrue(players.size() == 1);
+	}
 }
--- src/edu/psu/ist412/test/TableTest.java
+++ src/edu/psu/ist412/test/TableTest.java
@@ -1,9 +1,17 @@
 package edu.psu.ist412.test;
 
 import junit.framework.TestCase;
-
+import edu.psu.ist412.poker.Card;
+import edu.psu.ist412.poker.CardSuit;
+import edu.psu.ist412.poker.CardValue;
+import edu.psu.ist412.poker.Table;
+/**
+ * JUnit test for the Table class/methods
+ * @author PEdmonston
+ */
 public class TableTest extends TestCase {
-
+	Table tbl = new Table();
+	
 	public TableTest(String name) {
 		super(name);
 	}
@@ -15,5 +23,23 @@ public class TableTest extends TestCase {
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
+	public void testTable() {
+		CardSuit s = new CardSuit("Hearts","H");
+		CardValue v = new CardValue("10","T",10);
+		Card c = new Card(s,v);
+		System.out.println(tbl.toString());
+		assertTrue(tbl.getCards().size() == 0);
+		try {
+			tbl.addCard(c);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue(tbl.getCards().size() == 1);
+//		assertTrue(tbl.getFlop().size() == 3);
+//		assertTrue(tbl.getTurn().size() == 4);
+//		assertTrue(tbl.getRiver().size() == 5);
 
+		
+	}
 }
