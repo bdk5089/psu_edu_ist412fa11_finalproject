16cf40da66c12599fbd982b48a9b66c9f8dd4b1d
Pam Edmonston <pss138@gmail.com>
2011-11-29 11:49:10 -0500

    Junit test for Game class/methods

--- src/edu/psu/ist412/test/GameTest.java
+++ src/edu/psu/ist412/test/GameTest.java
@@ -1,9 +1,22 @@
 package edu.psu.ist412.test;
 
-import junit.framework.TestCase;
+import java.util.ArrayList;
 
+import junit.framework.TestCase;
+import edu.psu.ist412.poker.Game;
+import edu.psu.ist412.poker.Player;
+/**
+ * JUnit test for the Game classes/methods
+ * @author PEdmonston
+ */
 public class GameTest extends TestCase {
 
+	Player p1 = new Player(0,10,true);
+	Player p2 = new Player(1,10,false);		
+	ArrayList<Player> players = new ArrayList<Player>();
+//? players.add(p1);
+//?	players.add(p2);
+	Game g = new Game(players);
 	public GameTest(String name) {
 		super(name);
 	}
@@ -13,5 +26,27 @@ public class GameTest extends TestCase {
 
 	protected void tearDown() throws Exception {
 	}
+	public void testGame() {
+//		System.out.println("Game = " + g.toString());
+//		System.out.println("Players = " + g.getPlayers());
+//		System.out.println("Table before start = " + g.getTable());
+		g.startGame();
+		g.startRound();
+//		System.out.println("Table after startRound = " + g.getTable().toString());
+		g.dealHands();
+//		System.out.println("TableCards after dealHands = " + g.getTableCards().toString());
+//		System.out.println("Table after dealHands = " + g.getTable().toString());
+		assertTrue(g.getTable().getCards().size() == 0);
+		g.dealFlop();
+//		System.out.println("Table after dealFlop = " + g.getTable().toString());
+		assertTrue(g.getTable().getCards().size() == 3);
+		g.dealTurn();
+//		System.out.println("Table after dealTurn = " + g.getTable().toString());
+		assertTrue(g.getTable().getCards().size() == 4);
+		g.dealRiver();
+//		System.out.println("Table after dealRiver = " + g.getTable().toString());
+		assertTrue(g.getTable().getCards().size() == 5);
+		assertTrue(g.getState().toString() == "START");
 
+	}
 }
